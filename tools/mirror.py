9011479b878be27678d3ee16e99749322e99ae32
Allan Boll <allan@acoby.com>
2020-05-06 18:39:43 -0700

    mirror script, separate overwrite and delete option

--- tools/mirror.py
+++ tools/mirror.py
@@ -12,7 +12,7 @@ import time
 import random
 import string
 
-def mirror(address, localDir, collection, deleteAndOverwrite):
+def mirror(address, localDir, collection, overwrite, delete, deleteConfirmed):
     # Log in.
     authtoken = ''.join(random.choice(string.ascii_lowercase) for i in range(50))
     pin = random.randint(1000,9999)
@@ -54,7 +54,7 @@ def mirror(address, localDir, collection, deleteAndOverwrite):
         missing = image["name"] not in localFiles
         wrongSize = not missing and getsize(path) != image["size"]
         wrongTimestamp = not missing and abs(getmtime(path) - image["dateModified"]) > 4  # Windows may have some granularity issues, so using this tolerance
-        if missing or (deleteAndOverwrite and (wrongSize or wrongTimestamp)):
+        if missing or (overwrite and (wrongSize or wrongTimestamp)):
             log("Downloading " + image["name"])
             req = Request("http://" + address + "/api/images/" + str(image["imageId"]) + "?size=full")
             req.add_header("Cookie", "authtoken=" + authtoken)
@@ -70,17 +70,49 @@ def mirror(address, localDir, collection, deleteAndOverwrite):
             log("Downloaded with " + "{:.2f}".format(mbPerSec) + " MiB/sec")
             utime(path, (image["dateModified"], image["dateModified"]))
 
-    if deleteAndOverwrite:
+    if delete or deleteConfirmed:
         extraFiles = list(set(localFiles)-set(remoteFiles))
-        for file in extraFiles:
-            path = join(localDir, file)
-            log("Deleting extra local file " + path)
-            remove(path)
+        if len(extraFiles) > 0:
+            for file in extraFiles:
+                path = join(localDir, file)
+                log("Extra local file: " + path)
+
+            confirmed = deleteConfirmed
+            if not confirmed:
+                confirmed = confirm("Delete extra local files?")
+
+            if confirmed:
+                for file in extraFiles:
+                    path = join(localDir, file)
+                    log("Deleting extra local file: " + path)
+                    remove(path)
 
     log("Done")
 
 def log(msg):
     print("[" + datetime.utcnow().isoformat() + "] " + msg)
+
+# https://code.activestate.com/recipes/541096-prompt-the-user-for-confirmation/
+def confirm(prompt=None, resp=False):
+    if prompt is None:
+        prompt = 'Confirm'
+
+    if resp:
+        prompt = '%s [%s]|%s: ' % (prompt, 'y', 'n')
+    else:
+        prompt = '%s [%s]|%s: ' % (prompt, 'n', 'y')
+
+    while True:
+        ans = raw_input(prompt)
+        if not ans:
+            return resp
+        if ans not in ['y', 'Y', 'n', 'N']:
+            print 'please enter y or n.'
+            continue
+        if ans == 'y' or ans == 'Y':
+            return True
+        if ans == 'n' or ans == 'N':
+            return False
 
 def main():
     dirExample = "/home/somebody/Pictures/Phone"
@@ -91,9 +123,11 @@ def main():
     parser.add_argument("--address", required=True, help="The IP and port of your Android phone running WifiPhotos. Example: 192.168.1.123:8080")
     parser.add_argument("--localdir", required=True, help="Local directory to mirror to. Example: " + dirExample)
     parser.add_argument("--collection", default="Camera", help="Name of collection to mirror. Example: Camera")
-    parser.add_argument("--delete-and-overwrite", dest="deleteAndOverwrite", action="store_true", help="Whether to delete extra files locally and overwrite local files if size or timestamp differs from remote.")
+    parser.add_argument("--overwrite", dest="overwrite", action="store_true", help="Whether to overwrite any local files with the same name if size or timestamp differs from remote.")
+    parser.add_argument("--delete", dest="delete", action="store_true", help="Whether to delete extra files locally that are not present remote.")
+    parser.add_argument("--delete-skip-confirm", dest="deleteConfirmed", action="store_true", help="Assume 'yes' to confirm to delete extra files locally that are not present remote.")
     args = parser.parse_args()
-    mirror(args.address, args.localdir, args.collection, args.deleteAndOverwrite)
+    mirror(args.address, args.localdir, args.collection, args.overwrite, args.delete, args.deleteConfirmed)
 
 
 if __name__ == "__main__":
